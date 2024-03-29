b57ddf6165a9e603bedd6ce3bd0bf5f518b632f0
mason <masonwong1999@gmail.com>
2024-03-29 21:02:12 +1100

    module 1: Modified App.js to include 3 buttons and index.html to add the react image to it. Run npm install and npm start to see the react webpage.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React and how
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        ></link>
       </header>
+      <body>
+        <div class="container">
+          <div class="row well mb-3">
+            <div class="col-md-3 col-sm-3">
+              <button type="button" class="btn btn-secondary btn-lg disabled">
+                Add Inventory
+              </button>
+            </div>
+            <div class="col-md-3 col-sm-3">
+              <button type="button" class="btn btn-secondary btn-lg disabled">
+                Check Inventory
+              </button>
+            </div>
+            <div class="col-md-3 col-sm-3">
+              <button type="button" class="btn btn-secondary btn-lg disabled">
+                Sell Items
+              </button>
+            </div>
+          </div>
+
+          <div class="row well mb-3">
+            <div class="col-md-6">
+              <h5>There are 8 items in the inventory!!</h5>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
